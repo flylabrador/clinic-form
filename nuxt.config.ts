cbbde4e11a6d7c8a80cef84eb76d5498802b8a2b
designer <youandok@gmail.com>
2025-11-11 18:18:38 +0800

    3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,9 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-
+  app: {
+    baseURL: process.env.NUXT_PUBLIC_BASE_URL || '/', // Actions 會覆蓋為 /clinic-form/
+  },
   css: [
     'vuetify/styles',
     '@mdi/font/css/materialdesignicons.min.css'
